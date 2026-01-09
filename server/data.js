f7a22207c0fab48a8f70a6e7391c6c53360a998a
cortext-101 <wesleygaiski@gmail.com>
2026-01-09 12:48:15 -0500

    Create data.js

---
+++ server/data.js
@@ -0,0 +1,5 @@
+export const servers = {}; 
+// structure: { serverId: { name, channels: { channelId: { name, messages: [] } }, users: [] } }
+
+export const users = {}; 
+// { socketId: { username, currentServer, currentChannel } }
